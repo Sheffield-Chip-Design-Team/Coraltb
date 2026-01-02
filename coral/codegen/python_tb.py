5acc874ed0a1700e8f4ef18543d2fab1a456bcca
James <162901067+Enjimneering@users.noreply.github.com>
2026-01-02 00:20:32 +0000

    add combinational test generation

--- coral/codegen/python_tb.py
+++ coral/codegen/python_tb.py
@@ -1,33 +1,77 @@
 #!/usr/bin/env python3
 import argparse
+import logging
+
+from random import randint
+from jinja2 import Template
 from coral.common.pyverilog_helpers import *
 
-def generate_cocotb_test(module_name, ports, test_name):
+logger = logging.getLogger(__name__)
+
+#  TODO use jinja2 templates for test generation
+
+#  TODO decide if I should randomize the reset signal or not
+#  TODO add value constraint overrides to the port dictionary
+
+def generate_cocotb_test(module_name, ports, test_name, output_dir="tb") -> str:
     
-    clk_signal = next((p[2] for p in ports if any(x in p[2].lower() for x in ['clk', 'clock'])), None)
-    rst_signal = next((p[2] for p in ports if any(x in p[2].lower() for x in ['rst', 'reset'])), None)
-    
+    clk_signal  = find_port(ports, ["clk", "clock"])
+    rst_signal  = find_port(ports, ["rst", "reset"])
+
     sanity_name = test_name.removeprefix("test_")
+    test_file   = f"{output_dir}/{test_name.lower()}.py"
+    template    = ""
 
-    if not clk_signal or not rst_signal:
-        return f"#[TESTGEN] Cannot auto-generate cocotb test for {module_name}: clock or reset not found.\n"
+    if not clk_signal:
 
-    print(f"[TESTGEN] clock and reset found in {module_name}: {clk_signal}, {rst_signal}")
+        logger.info(f"generating combinational sanity test for {module_name}: (clock not found)")
 
-    # Check if reset signal is active-low (ends with '_n')
-    active_low_reset = rst_signal.lower().endswith('_n')
-    if (active_low_reset):
-        start_rst = 0
-        end_rst = 1
-    else:
-        start_rst = 1
-        end_rst = 0
+        input_ports = {}
+        input_ports, _ , _ = filter_ports_by_direction(ports)
+        clk_port = find_port(ports, ["clk", "clock"])
 
-    test_file = f"tb/{test_name.lower()}.py"
-    template = f"""import cocotb
+        # exclude the clock input port from randomization
+        if clk_port in input_ports:
+            logger.debug(f"Excluding clock port {clk_port} from input randomization.")
+            input_ports = input_ports.pop(clk_port)
+        
+        template = f"""import cocotb
+from random import randint
+from cocotb.triggers import Timer
+
+async def test_{sanity_name}_combinational(uut):
+  # set random input values
+"""
+        for name, info in input_ports.items():
+
+            max_value = max(2**evaluate_width_expr(str(info['width']))-1,1)
+            template += f'  uut.{name}.value = randint(0,{max_value})\n'
+            template += f'  uut._log.info("Setting {name} to {{uut.{name}.value}}")\n\n'
+            
+        template += f"""  await Timer(randint(1,10), unit="ns")
+  
+  # TODO - add checks for expected output values
+
+  uut._log.info("Test Complete!")""" 
+
+    elif rst_signal:
+
+        logger.info(f"clock and reset found in {module_name}: {clk_signal}, {rst_signal}")
+
+        # Check if reset signal is active-low (ends with '_n')
+        active_low_reset = rst_signal.lower().endswith('_n')
+        if (active_low_reset):
+            start_rst = 0
+            end_rst = 1
+        else:
+            start_rst = 1
+            end_rst = 0
+
+        template = f"""import cocotb
 from random import randint
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles, RisingEdge
+import os
 
 async def reset(uut, reset_duration=randint(1,10)):
     # assert reset
@@ -42,7 +86,7 @@ async def test_{sanity_name}_sanity(uut):
     clock = Clock(uut.{clk_signal}, 40, units="ns")
     cocotb.start_soon(clock.start())
     await ClockCycles(uut.{clk_signal}, 1)
-   
+
     # reset the module
     await reset(uut)
     await RisingEdge(uut.{clk_signal})
@@ -51,56 +95,21 @@ async def test_{sanity_name}_sanity(uut):
     await ClockCycles(uut.{clk_signal}, 100)
     uut._log.info("Test Complete!")
 
-"""
+    """
+
+    else:
+        logger.info(f"generating clock-only sanity test for {module_name}: (reset not found)")
+        template = f""
+        
+    os.makedirs(output_dir, exist_ok=True)
     
     with open(test_file, "w") as f:
         f.write(template)
-    return f"# Generated cocotb test template: {test_file}"
+    
+    logger.info(f"Generated cocotb test template {test_file}.")
+    return f"Generated cocotb test template: {test_file}"
+
 
 if __name__ == "__main__":
-    
-    # parser = argparse.ArgumentParser(description="Generate cocotb test templates.")
-    # parser.add_argument("verilog_files", nargs="+", help="List of Verilog files")
-    # parser.add_argument("-name", type=str, default=None, help="Custom test filename")
-    # args = parser.parse_args()
-
-    # if len(sys.argv) < 2:
-    #     print("Usage: python3 instantiate.py <verilog_files> [-name=<test_name>]")
-    #     sys.exit(1)
-
-    # all_args = " ".join(sys.argv[1:])
-   
-    # # Filter out any -name argument and its value
-    # filtered_args = []
-    # skip_next = False
-    # for arg in all_args.split():
-    #     if arg.startswith("-name"):
-    #         skip_next = True
-    #         continue
-    #     if skip_next:
-    #         skip_next = False
-    #         continue
-    #     filtered_args.append(arg)
-    # file_list = [f.strip() for f in " ".join(filtered_args).split("\n") if f.strip()]
-
-    # if not file_list:
-    #     print("No Verilog files found")
-    #     sys.exit(1)
-
-    # try:
-    #     ast, _ = s(file_list, debug=False)
-    # except Exception as e:
-    #     print(f"Error parsing files: {e}")
-    #     sys.exit(1)
-
-    # declared_signals = set()
-    # modules = extract_module_info(ast)
-    
-    # test_filename = args.name
-    # for module_name, ports in modules:
-    #     if test_filename:
-    #         generate_cocotb_test(module_name, ports, test_filename)
-    #     else:
-    #         generate_cocotb_test(module_name, ports, module_name.lower())
-    
+    # Example usage
     cleanup_pyverilog_artifacts()
