14e18fa77b4526b685b0fcd9a6ebfef186e1d8a2
James <162901067+Enjimneering@users.noreply.github.com>
2026-02-25 22:46:23 +0000

    Update simulation run logic and cleanup
    Refactor simulation run path and clean build directory - fix verilator WNO timescale warning

--- coral/run/sim.py
+++ coral/run/sim.py
@@ -4,38 +4,15 @@ from cocotb_tools.runner import Runner
 import os
 import sys
 
-# class Tee:
-#     def __init__(self, *streams):
-#         self.streams = streams
-
-#     def write(self, data):
-#         for s in self.streams:
-#             s.write(data)
-#             s.flush()
-
-#     def flush(self):
-#         for s in self.streams:
-#             s.flush()
-
-# orig_execute = Runner._execute
-
-# def tee_execute(self, cmds, cwd):
-#     if self.log_file is None:
-#         return orig_execute(self, cmds, cwd)
-
-#     with open(self.log_file, "w") as f:
-#         tee = Tee(sys.stdout, f)
-#         self._execute_cmds(cmds, cwd, tee)
-
-# Runner._execute = tee_execute
-
 from cocotb_tools.runner import get_runner
 
 def run_simulation(simulator="icarus", wtb_top="wtb", src_dir="", rtl_sources=[], test_module="", test_dir=None, waves=True, coverage=False, output_dir=None):
     """Run a cocotb simulation using the specified simulator."""
 
-    sim_build_dir   = (output_dir+"/"+simulator) if output_dir else simulator
-    test_output_dir = sim_build_dir+"/"+test_module
+    sim_build_dir = (output_dir+"/"+simulator+"/build") if output_dir else simulator
+    test_output_dir = sim_build_dir+"/"+"results"
+  
+    build_path = Path(sim_build_dir)
 
     os.environ["COCOTB_LOG_LEVEL"] = "INFO"      # or DEBUG
 
@@ -55,18 +32,21 @@ def run_simulation(simulator="icarus", wtb_top="wtb", src_dir="", rtl_sources=[]
         if not os.path.exists(source):
             print(f"[ERROR] Source file {source} does not exist.")
             return
-        
-    # if not os.path.isabs(test_module):
-    #     if not os.path.isabs(test_module):
-    #         test_module_path = os.path.join(test_module_dir, test_module+)
-    #         if not os.path.exists(test_module_path):
-    #             print(f"[ERROR] Test file {test_module} does not exist.")
-    #             return
-            
+    
+    import shutil
+    if os.path.exists(build_path):
+        shutil.rmtree(build_path)
+   
+    build_args = []
+    if simulator == "verilator":
+        build_args = ['--Wno-TIMESCALEMOD']
+
     runner.build(
+        clean=True,
         verilog_sources=rtl_sources,
         hdl_toplevel=wtb_top,
         build_dir=sim_build_dir,
+        build_args = build_args
     )
 
     runner.test(
@@ -77,6 +57,15 @@ def run_simulation(simulator="icarus", wtb_top="wtb", src_dir="", rtl_sources=[]
         test_module=test_module,
         test_dir=test_output_dir
     )
+    
     script_run_from = os.getcwd()
-    info(f"Simulation completed: log in {script_run_from}/{test_output_dir}/latest.log")
+
+    # Display the output log
+    logfile_path = f"{script_run_from}/{test_output_dir}/latest.log"
+    with open(logfile_path, "r") as f:
+        for line in f:
+            print(line, end="") 
+
+    info(f"Simulation completed: see log in {logfile_path}")
+
     
